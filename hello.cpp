845c075237cf45a30b2e06d4749aafabb99ace77
rickyjames1750 <rickyjames1750@gmail.com>
2021-06-08 22:56:01 -0700

    This commit will create c++ if statements

--- hello.cpp
+++ hello.cpp
@@ -62,13 +62,37 @@ int main(){
   
   // Correct order of operations
   cout << "1 + 2 - 3 * 2 = " << 1 + 2 - 3 * 2 << endl;
-  // we can see here it is important to use braces all the time or else it will result in a wrong answer possibly like below
+  // We can see here it is important to use braces all the time or else it will result in a wrong answer possibly like below
   cout << "(1 + 2 -3) * 2 = " << (1 + 2 - 3) * 2 << endl;
 
   cout << "4 / 5 = " << 4 / 5 << endl; 
-  // performing casting 
+  // Performing casting 
   cout << "4 / 5 = " << (float) 4 / 5 << endl; 
 
+// If statements 
+// Comparison operators == , != , > , < , >= , <=
+// Logical operators && , || , !
 
+int age = 70;
+int ageAtLastExam = 16; 
+bool isNotIntoxicated = true;
+
+if((age >= 1) && (age < 16)){
+
+  cout << "You can't drive" << endl;  
+
+} else if(! isNotIntoxicated){
+
+  cout << "You can't drive" << endl;
+
+} else if(age >= 80 && ((age > 100) || ((age - ageAtLastExam) > 5))){
+
+  cout << "You can't drive" << endl;
+
+} else {
+  cout << "You can drive" << endl; 
+
+}
   return 0; 
+
 }
