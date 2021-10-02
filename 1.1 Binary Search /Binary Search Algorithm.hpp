632e1be572f69182670d4f2b2e05b10af3721c71
rickyjames1750 <rickyjames1750@gmail.com>
2021-10-01 19:57:40 -0700

    Create Binary Search Algorithm.hpp

---
+++ 1.1 Binary Search /Binary Search Algorithm.hpp
@@ -0,0 +1,13 @@
+//
+//  Binary Search Algorithm.hpp
+//  
+//
+//  Created by Ricky Sparks on 10/1/21.
+//
+
+#ifndef Binary_Search_Algorithm_hpp
+#define Binary_Search_Algorithm_hpp
+
+#include <stdio.h>
+
+#endif /* Binary_Search_Algorithm_hpp */
