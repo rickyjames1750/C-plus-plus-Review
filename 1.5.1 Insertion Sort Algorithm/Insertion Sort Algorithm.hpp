7c4d57021786dd371f56b2ccf495167912b2ceaf
rickyjames1750 <rickyjames1750@gmail.com>
2021-10-03 14:38:46 -0700

    Create Insertion Sort Algorithm.hpp

---
+++ 1.5.1 Insertion Sort Algorithm/Insertion Sort Algorithm.hpp
@@ -0,0 +1,13 @@
+//
+//  Insertion Sort Algorithm.hpp
+//  
+//
+//  Created by Ricky Sparks on 10/2/21.
+//
+
+#ifndef Insertion_Sort_Algorithm_hpp
+#define Insertion_Sort_Algorithm_hpp
+
+#include <stdio.h>
+
+#endif /* Insertion_Sort_Algorithm_hpp */
